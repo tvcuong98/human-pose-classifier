bbf058e678caa42702f66d4aabfc0b53072306ce
tvcuong98 <unikool8989@gmail.com>
2024-05-07 12:18:37 +0700

    Revert "adding multi-gpu for kinetic-gan, but still not done fixing the cuda error"
    This reverts commit 9a697fbd56b719af5a52d4333cc9158ab0132a3b.

--- Kinetic-GAN-2D-SAN/training-train.sh
+++ Kinetic-GAN-2D-SAN/training-train.sh
@@ -3,12 +3,13 @@
 
 
 # Execute the Python script with provided parameters
-python3 kinetic-gan-multi-gpu.py \
+python3 train.py \
   --b1 0.5 \
   --b2 0.999 \
   --batch_size 128 \
   --channels 2 \
-  --checkpoint_interval 40527 \
+  --checkpoint_interval 100 \
+  --lambda_gp 10 \
   --dataset h36m \
   --latent_dim 512 \
   --mlp_dim 8 \
@@ -17,9 +18,11 @@ python3 kinetic-gan-multi-gpu.py \
   --n_cpu 8 \
   --n_critic 5 \
   --n_epochs 4000 \
-  --sample_interval 40527 \
+  --sample_interval 100 \
   --t_size 1 \
   --v_size 16 \
   --csv_path "../data/kp_16_cover_modes/cover2/traincover2.csv" \
-  --runs "runs_cover2" \
-  --tb_runs "tensorboard_runs_cover2"
+  --runs "runs_cover2_test" \
+  --tb_runs "tensorboard_runs_cover2_test" \
+  --device 0 \
+  --model "san"
